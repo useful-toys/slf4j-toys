e0bfc4af34558fc8f1890ced851381dbb2f9f345
Daniel Felix Ferber <danielferber@yahoo.com.br>
2014-06-12 01:58:56 -0300

    [tst] Better unit test for long int formatter. And fixes.

--- slf4j-toys/src/main/java/br/com/danielferber/slf4jtoys/slf4j/utils/UnitFormatter.java
+++ slf4j-toys/src/main/java/br/com/danielferber/slf4jtoys/slf4j/utils/UnitFormatter.java
@@ -15,8 +15,6 @@
  */
 package br.com.danielferber.slf4jtoys.slf4j.utils;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  *
  * @author Daniel Felix Ferber
@@ -26,51 +24,49 @@ public final class UnitFormatter {
     private UnitFormatter() {
     }
 
-    private static final double[] TIME_FACTORS = new double[]{1000.0, 1000.0, 1000.0, 60.0, 60.0};
+    private static final int[] TIME_FACTORS = new int[]{1000, 1000, 1000, 60, 60};
     private static final String[] TIME_UNITS = new String[]{"ns", "us", "ms", "s", "m", "h"};
     private static final String[] MEMORY_UNITS = new String[]{"B", "kB", "MB", "GB"};
-    private static final double[] MEMORY_FACTORS = new double[]{1000.0, 1000.0, 1000.0};
+    private static final int[] MEMORY_FACTORS = new int[]{1000, 1000, 1000};
     private static final String[] ITERATIONS_PER_TIME_UNITS = new String[]{"/s", "k/s", "M/s"};
-    private static final double[] ITERATIONS_PER_TIME_FACTORS = new double[]{1000.0, 1000.0, 1000.0};
+    private static final int[] ITERATIONS_PER_TIME_FACTORS = new int[]{1000, 1000, 1000};
     private static final String[] ITERATIONS_UNITS = new String[]{"", "k", "M"};
-    private static final double[] ITERATIONS_FACTORS = new double[]{1000.0, 1000.0, 1000.0};
+    private static final int[] ITERATIONS_FACTORS = new int[]{1000, 1000, 1000};
 
-    private static String longUnit(long lvalue, String[] timeUnits, double[] timeFactors) {
-        if (lvalue == 0.0) {
-            return "0" + timeUnits[0];
-        }
-
+    static String longUnit(long value, String[] units, int[] factors) {
         int index = 0;
-        double limit = timeFactors[index] * 1.1;
-        double modifiedValue = lvalue;
-        if (modifiedValue <= limit) {
-            return String.format("%d%s", lvalue, timeUnits[index]);
+        int limit = factors[index] + factors[index] / 10;
+        if (value < limit) {
+            return String.format("%d%s", value, units[index]);
         }
 
-        int last = timeUnits.length - 1;
+        int last = units.length - 1;
+        double doubleValue;
         do {
-            modifiedValue /= timeFactors[index];
-            limit = timeFactors[index] * 1.1;
+            doubleValue = (double) value / (double) factors[index];
+            value /= factors[index];
+            limit = factors[index];
             index++;
-        } while (index != last && modifiedValue > limit);
-        return String.format("%.1f%s", modifiedValue, timeUnits[index]);
+        } while (index != last && value >= limit);
+
+        return String.format("%.1f%s", doubleValue, units[index]);
     }
 
-    private static String doubleUnit(double value, String[] timeUnits, double[] timeFactors) {
+    static String doubleUnit(double value, String[] units, int[] factors) {
         if (value == 0.0) {
-            return "0" + timeUnits[0];
+            return "0" + units[0];
         }
 
-        int last = timeUnits.length - 1;
+        int last = units.length - 1;
         int index = 0;
-        double limit = timeFactors[index] * 1.1;
+        double limit = factors[index] * 1.1;
         double modifiedValue = value;
         while (index != last && modifiedValue > limit) {
-            modifiedValue /= timeFactors[index];
-            limit = timeFactors[index] * 1.1;
+            modifiedValue /= factors[index];
+            limit = factors[index] * 1.1;
             index++;
         }
-        return String.format("%.1f%s", modifiedValue, timeUnits[index]);
+        return String.format("%.1f%s", modifiedValue, units[index]);
     }
 
     public static String bytes(long value) {
@@ -85,8 +81,16 @@ public final class UnitFormatter {
         return doubleUnit(value, TIME_UNITS, TIME_FACTORS);
     }
 
+    public static String iterations(long value) {
+        return longUnit(value, ITERATIONS_UNITS, ITERATIONS_FACTORS);
+    }
+
     public static String iterations(double value) {
         return doubleUnit(value, ITERATIONS_UNITS, ITERATIONS_FACTORS);
+    }
+
+    public static String iterationsPerSecond(long value) {
+        return longUnit(value, ITERATIONS_PER_TIME_UNITS, ITERATIONS_PER_TIME_FACTORS);
     }
 
     public static String iterationsPerSecond(double value) {
--- slf4j-toys/src/test/java/br/com/danielferber/slf4jtoys/slf4j/utils/UnitFormatterTest.java
+++ slf4j-toys/src/test/java/br/com/danielferber/slf4jtoys/slf4j/utils/UnitFormatterTest.java
@@ -17,23 +17,46 @@ public class UnitFormatterTest {
     public UnitFormatterTest() {
     }
 
+    private static final int[] FACTORS = new int[]{1000, 1000, 1000};
+    private static final String[] UNITS = new String[]{"A", "B", "C"};
+
     @Test
-    public void testBytes() {
-        assertEquals("0B", UnitFormatter.bytes(0));
-        assertEquals("10B", UnitFormatter.bytes(10));
-        assertEquals("100B", UnitFormatter.bytes(100));
-        assertEquals("1000B", UnitFormatter.bytes(1000));
-        assertEquals("1099B", UnitFormatter.bytes(1099));
-        assertEquals("1100B", UnitFormatter.bytes(1100));
-        assertEquals("1,1kB", UnitFormatter.bytes(1101));
-        assertEquals("10,0kB", UnitFormatter.bytes(10000));
-        assertEquals("10,1kB", UnitFormatter.bytes(10100));
-        assertEquals("100,0kB", UnitFormatter.bytes(100000));
-        assertEquals("1000,0kB", UnitFormatter.bytes(1000000));
-        assertEquals("10,0MB", UnitFormatter.bytes(10000000));
-        assertEquals("10,1MB", UnitFormatter.bytes(10100000));
-        assertEquals("100,0MB", UnitFormatter.bytes(100000000));
-        assertEquals("1000,0MB", UnitFormatter.bytes(1000000000));
+    public void testLongUnit() {
+        assertEquals("0A", UnitFormatter.longUnit(0, UNITS, FACTORS));
+        assertEquals("1A", UnitFormatter.longUnit(1, UNITS, FACTORS));
+        assertEquals("9A", UnitFormatter.longUnit(9, UNITS, FACTORS));
+        assertEquals("10A", UnitFormatter.longUnit(10, UNITS, FACTORS));
+        assertEquals("11A", UnitFormatter.longUnit(11, UNITS, FACTORS));
+        assertEquals("99A", UnitFormatter.longUnit(99, UNITS, FACTORS));
+        assertEquals("100A", UnitFormatter.longUnit(100, UNITS, FACTORS));
+        assertEquals("101A", UnitFormatter.longUnit(101, UNITS, FACTORS));
+        assertEquals("999A", UnitFormatter.longUnit(999, UNITS, FACTORS));
+        assertEquals("1000A", UnitFormatter.longUnit(1000, UNITS, FACTORS));
+        assertEquals("1001A", UnitFormatter.longUnit(1001, UNITS, FACTORS));
+        assertEquals("1099A", UnitFormatter.longUnit(1099, UNITS, FACTORS));
+        assertEquals("1,1B", UnitFormatter.longUnit(1100, UNITS, FACTORS));
+        assertEquals("1,1B", UnitFormatter.longUnit(1101, UNITS, FACTORS));
+        assertEquals("1,1B", UnitFormatter.longUnit(1149, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1150, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1151, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1199, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1200, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1201, UNITS, FACTORS));
+        assertEquals("1,2B", UnitFormatter.longUnit(1249, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1250, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1251, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1299, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1300, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1301, UNITS, FACTORS));
+        assertEquals("1,3B", UnitFormatter.longUnit(1349, UNITS, FACTORS));
+        assertEquals("4,9B", UnitFormatter.longUnit(4900, UNITS, FACTORS));
+        assertEquals("4,9B", UnitFormatter.longUnit(4949, UNITS, FACTORS));
+        assertEquals("5,0B", UnitFormatter.longUnit(4990, UNITS, FACTORS));
+        assertEquals("5,0B", UnitFormatter.longUnit(5000, UNITS, FACTORS));
+        assertEquals("5,0B", UnitFormatter.longUnit(5010, UNITS, FACTORS));
+        assertEquals("5,1B", UnitFormatter.longUnit(5050, UNITS, FACTORS));
+        assertEquals("999,9B", UnitFormatter.longUnit(999900, UNITS, FACTORS));
+        assertEquals("1,0C", UnitFormatter.longUnit(1000000, UNITS, FACTORS));
     }
 
 }
