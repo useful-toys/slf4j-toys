b7dcd9393988ac64f21552eb997e9713d7e27d57
Daniel Felix Ferber <danielferber@gmail.com>
2017-02-22 23:00:39 -0300

    [bld] Automatic license fixing.

--- slf4j-toys/src/main/java/org/usefultoys/slf4j/report/Reporter.java
+++ slf4j-toys/src/main/java/org/usefultoys/slf4j/report/Reporter.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2015 Daniel Felix Ferber.
+/**
+ * Copyright 2017 Daniel Felix Ferber
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
