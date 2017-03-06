5d87f106138d59a5c50538c1d9470a7d5c1e17f4
Daniel Felix Ferber <danielferber@gmail.com>
2017-03-05 21:22:43 -0300

    [doc] Fixed license headers.

--- examples/src/main/java/org/usefultoys/slf4j/examples/ExampleCommons.java
+++ examples/src/main/java/org/usefultoys/slf4j/examples/ExampleCommons.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2017 Daniel Felix Ferber
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,11 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package org.usefultoys.slf4j.examples;
 
