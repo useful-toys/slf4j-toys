ded669c59a99cf1747d2c9238f6bd39c4b3499e7
danielferber <danielferber@gmail.com>
2019-09-25 23:01:25 -0300

    [doc] Update license headers.

--- slf4j-toys/src/test/java/org/slf4j/impl/TestLoggerFactory.java
+++ slf4j-toys/src/test/java/org/slf4j/impl/TestLoggerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Daniel Felix Ferber
+ * Copyright 2019 Daniel Felix Ferber
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
