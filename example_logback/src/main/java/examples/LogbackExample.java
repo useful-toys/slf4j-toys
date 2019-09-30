6759678a853f8d365415dae313b7899eb9cf08d0
X7WS <danielferber@gmail.com>
2019-09-30 19:48:55 -0300

    [dev] Removed duplicated code on constructor.

---
+++ example_logback/src/main/java/examples/LogbackExample.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2019 Daniel Felix Ferber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package examples;
+
+import org.slf4j.Logger;
+import org.usefultoys.slf4j.LoggerFactory;
+import org.usefultoys.slf4j.meter.Meter;
+import org.usefultoys.slf4j.meter.MeterFactory;
+
+/**
+ *
+ * @author Daniel Felix Ferber
+ */
+public class LogbackExample {
+
+    public static final Logger logger = LoggerFactory.getLogger("example");
+
+    static {
+        /* Enable managed bean that is able to read CPU usage.  */
+        System.setProperty("profiler.usePlatformManagedBean", "true");
+    }
+
+    public static void main(final String argv[]) {
+        example1();
+        example2A();
+        example2C();
+        example2B();
+        example2D();
+        example3A();
+        example3B();
+        example3C();
+    }
+
+    private static void example1() {
+        /* Simplistic usage. */
+        final Meter m1 = MeterFactory.getMeter(logger, "operation1").start();
+        runOperation(true);
+        m1.ok();
+    }
+
+    private static void example2A() {
+        /* Report execution flow with Meter.ok() with string parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation2").start();
+        // Check if user exists in database
+        boolean exist = runOperation(true);
+        if (exist) {
+            // Update user in database
+            someOperation();
+            m2.ok("Update");
+        } else {
+            // Insert user into database
+            otherOperation();
+            m2.ok("Insert");
+        }
+    }
+
+    static enum OperationResult {
+
+        UPDATE, INSERT
+    }
+
+    private static void example2C() {
+        /* Report execution flow with Meter.ok() with enum parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation2").start();
+        // Check if user exists in database
+        boolean exist = runOperation(true);
+        if (exist) {
+            // Update user in database
+            someOperation();
+            m2.ok(OperationResult.UPDATE);
+        } else {
+            // Insert user into database
+            otherOperation();
+            m2.ok(OperationResult.INSERT);
+        }
+    }
+
+    private static void example2B() {
+        /* Report execution flow with Meter.flow() with string parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation2").start();
+        // Check if user exists in database
+        boolean exist = runOperation(true);
+        if (exist) {
+            // Update user in database
+            someOperation();
+            m2.flow("Update");
+        } else {
+            // Insert user into database
+            otherOperation();
+            m2.flow("Insert");
+        }
+        m2.ok();
+    }
+
+    private static void example2D() {
+        /* Report execution flow with Meter.flow() with enum parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation2").start();
+        // Check if user exists in database
+        boolean exist = runOperation(true);
+        if (exist) {
+            // Update user in database
+            someOperation();
+            m2.flow(OperationResult.UPDATE);
+        } else {
+            // Insert user into database
+            otherOperation();
+            m2.flow(OperationResult.INSERT);
+        }
+        m2.ok();
+    }
+
+    private static void example3A() {
+        /* Report execution flow with Meter.ok() and Meter.reject(). */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation3").start();
+        // Check if user has permissions
+        boolean authorized = runOperation(false);
+        if (authorized) {
+            // Proceed
+            someOperation();
+            m2.ok("Update");
+        } else {
+            m2.reject("Unauthorized");
+        }
+    }
+
+    static enum OperationFailure {
+
+        UNAUTHORIZED
+    }
+
+    private static void example3B() {
+        /* Report execution flow with Meter.ok() and Meter.reject() with enum parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation3").start();
+        // Check if user has permissions
+        boolean authorized = runOperation(false);
+        if (authorized) {
+            // Proceed
+            someOperation();
+            m2.ok("Update");
+        } else {
+            m2.reject(OperationFailure.UNAUTHORIZED);
+        }
+    }
+
+    public static class UnauthorizedException extends Exception {
+    }
+
+    private static void example3C() {
+        /* Report execution flow with Meter.ok() and Meter.reject() with exception parameter. */
+        final Meter m2 = MeterFactory.getMeter(logger, "operation3").start();
+        try {
+            // Check if user has permissions
+            boolean authorized = runOperation(false);
+            if (!authorized) {
+                throw new UnauthorizedException();
+            }
+            // Proceed
+            someOperation();
+            m2.ok("Update");
+        } catch (UnauthorizedException e) {
+            m2.reject(e);
+        }
+    }
+
+    private static boolean runOperation(boolean expectedResult) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            // ignore
+        }
+        return expectedResult;
+    }
+
+    private static void someOperation() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            // ignore
+        }
+    }
+
+    private static void otherOperation() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            // ignore
+        }
+    }
+}
---
+++ slf4j-toys/src/main/java/org/usefultoys/slf4j/logback/MeterConverter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Daniel Felix Ferber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.usefultoys.slf4j.logback;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.slf4j.Marker;
+import org.usefultoys.slf4j.meter.Markers;
+
+public class MeterConverter extends ClassicConverter  {
+    public String convert(final ILoggingEvent event) {
+        final Marker marker = event.getMarker();
+        if (marker == Markers.MSG_START) return "START";
+        if (marker == Markers.MSG_PROGRESS) return "PROGRESS";
+        if (marker == Markers.MSG_OK) return "OK";
+        if (marker == Markers.MSG_SLOW_OK) return "SLOW";
+        if (marker == Markers.MSG_REJECT) return "REJECT";
+        if (marker == Markers.MSG_FAIL) return "FAIL";
+        if (marker == Markers.DATA_START) return "START";
+        if (marker == Markers.DATA_PROGRESS) return "PROGRESS";
+        if (marker == Markers.DATA_OK) return "OK";
+        if (marker == Markers.DATA_SLOW_OK) return "SLOW";
+        if (marker == Markers.DATA_REJECT) return "REJECT";
+        if (marker == Markers.DATA_FAIL) return "FAIL";
+        if (marker == Markers.BUG) return "BUG";
+        if (marker == Markers.ILLEGAL) return "ILLEGAL";
+        if (marker == Markers.INCONSISTENT_START) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_INCREMENT) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_PROGRESS) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_EXCEPTION) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_REJECT) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_OK) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_FAIL) return "INCONSISTENT";
+        if (marker == Markers.INCONSISTENT_FINALIZED) return "INCONSISTENT";
+        if (marker == org.usefultoys.slf4j.watcher.Markers.MSG_WATCHER) return "WATCHER";
+        if (marker == org.usefultoys.slf4j.watcher.Markers.DATA_WATCHER) return "WATCHER";
+        return event.getLevel().toString();
+    }
+}
