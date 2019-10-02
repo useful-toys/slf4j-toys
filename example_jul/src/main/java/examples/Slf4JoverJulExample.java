5827f2181fc7e99031d7d70c296b3f5a4f2e65e7
X7WS <danielferber@gmail.com>
2019-10-01 23:21:48 -0300

    [dev] JUL example.

--- example_jul/src/main/java/examples/Slf4JoverJulExample.java
+++ example_jul/src/main/java/examples/Slf4JoverJulExample.java
@@ -17,14 +17,23 @@ package examples;
 
 import org.slf4j.Logger;
 import org.usefultoys.slf4j.LoggerFactory;
+import org.usefultoys.slf4j.SessionConfig;
 import org.usefultoys.slf4j.internal.SystemConfig;
+import org.usefultoys.slf4j.meter.Meter;
+import org.usefultoys.slf4j.meter.MeterConfig;
+import org.usefultoys.slf4j.meter.MeterFactory;
 import org.usefultoys.slf4j.watcher.WatcherConfig;
 import org.usefultoys.slf4j.watcher.WatcherSingleton;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 import java.util.logging.LogManager;
 
 /**
+ * Test SLF4J Meter using JUL as underlying framework.
+ * This examample demonstrates recommended logging setting for JUL.
+ *
  * @author Daniel Felix Ferber
  */
 public class Slf4JoverJulExample {
@@ -32,15 +41,28 @@ public class Slf4JoverJulExample {
     public static final Logger logger = LoggerFactory.getLogger("example");
 
     static {
+        java.util.Locale.setDefault(Locale.ENGLISH);
         System.setProperty("java.util.logging.config.file",
                 Slf4JoverJulExample.class.getClassLoader().getResource("log.properties").getFile());
-        WatcherConfig.delayMilliseconds = 500;
+        SessionConfig.uuidSize = 6;
+        WatcherConfig.delayMilliseconds = 1000;
         WatcherConfig.periodMilliseconds = 500;
+        SystemConfig.useClassLoadingManagedBean = true;
+        SystemConfig.useCompilationManagedBean = true;
+        SystemConfig.useGarbageCollectionManagedBean = true;
+        SystemConfig.useMemoryManagedBean = true;
         SystemConfig.usePlatformManagedBean = true;
+        MeterConfig.progressPeriodMilliseconds=300;
     }
 
     public static void main(final String argv[]) throws IOException {
-        LogManager.getLogManager().readConfiguration();
+        InputStream inputStream = Slf4JoverJulExample.class.getResourceAsStream("/log.properties");
+        try {
+            LogManager.getLogManager().readConfiguration(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
         WatcherSingleton.startDefaultWatcherExecutor();
         runOperation(true);
@@ -48,10 +70,15 @@ public class Slf4JoverJulExample {
     }
 
     private static boolean runOperation(boolean expectedResult) {
-        try {
-            Thread.sleep(3000);
+        try (Meter m = MeterFactory.getMeter(logger, "runOperation").iterations(3).start()) {
+            Thread.sleep(1000);
+            m.inc().progress();
+            Thread.sleep(1000);
+            m.inc().progress();
+            Thread.sleep(1000);
+            m.inc().ok();
         } catch (InterruptedException ex) {
-            // ignore
+            ex.printStackTrace();
         }
         return expectedResult;
     }
