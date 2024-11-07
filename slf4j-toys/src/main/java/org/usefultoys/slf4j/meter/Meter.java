8ecba1d201f2a5a4582c6a5c5e10584db2825b3b
Daniel Felix Ferber <daniel.ferber@petrobras.com.br>
2024-11-07 14:49:37 -0300

    [doc] Update license headers.

--- slf4j-toys/src/main/java/org/usefultoys/slf4j/meter/Meter.java
+++ slf4j-toys/src/main/java/org/usefultoys/slf4j/meter/Meter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Daniel Felix Ferber
+ * Copyright 2024 Daniel Felix Ferber
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
