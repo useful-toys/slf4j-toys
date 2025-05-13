bef95e2fba6dd16e1991c28dcffff2b7b66c3ede
danielferber <danielferber@gmail.com>
2025-05-13 19:58:43 -0300

    test: add unit tests for ReportSystemEnvironment and security exception handling

---
+++ slf4j-toys/src/test/java/org/usefultoys/slf4j/report/ReportSystemEnvironmentTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2025 Daniel Felix Ferber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.usefultoys.slf4j.report;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.MockLogger;
+import org.usefultoys.slf4j.SessionConfig;
+import org.usefultoys.slf4j.SystemConfig;
+
+import java.nio.charset.Charset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ReportSystemEnvironmentTest {
+
+    private MockLogger mockLogger;
+
+    @BeforeAll
+    static void validate() {
+        assertEquals(Charset.defaultCharset().name(), SessionConfig.charset, "Test requires SessionConfig.charset = default charset");
+    }
+
+    @BeforeEach
+    void resetWatcherConfigBeforeEach() {
+        // Reinitialize each configuration to ensure a clean configuration before each test
+        ReporterConfig.reset();
+        SessionConfig.reset();
+        SystemConfig.reset();
+    }
+
+    @AfterAll
+    static void resetWatcherConfigAfterAll() {
+        // Reinitialize each configuration to ensure a clean configuration before each test
+        ReporterConfig.reset();
+        SessionConfig.reset();
+        SystemConfig.reset();
+    }
+
+    @BeforeEach
+    void setUp() {
+        final Logger logger = LoggerFactory.getLogger("test.report.sysprops");
+        mockLogger = (MockLogger) logger;
+        mockLogger.clearEvents();
+    }
+
+    @Test
+    void shouldLogSystemProperties() {
+        // Arrange
+        final ReportSystemEnvironment report = new ReportSystemEnvironment(mockLogger);
+
+        // Act
+        report.run();
+
+        // Assert
+        assertTrue(mockLogger.getEventCount() > 0);
+        final String logs = mockLogger.getEvent(0).getFormattedMessage();
+        assertTrue(logs.contains("PATH:"));
+    }
+}
