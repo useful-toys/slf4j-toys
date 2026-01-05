d50d9df21c1a38e0781a3a0d7c91ed9ce1d3eed7
Daniel Felix Ferber <daniel.ferber@petrobras.com.br>
2026-01-04 22:38:34 -0300

    test: rename MeterDataReadableMessageTest to ReadableMessageLegacyTest

--- src/test/java/org/usefultoys/slf4j/meter/ReadableMessageLegacyTest.java
+++ src/test/java/org/usefultoys/slf4j/meter/ReadableMessageLegacyTest.java
@@ -36,7 +36,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MeterDataReadableMessageTest {
+public class ReadableMessageLegacyTest {
 
     
 
