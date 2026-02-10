50778bbe978791568594d72c4cd72d71c9559aa3
Daniel Felix Ferber <daniel.ferber@petrobras.com.br>
2026-02-10 14:38:05 -0300

    Update copyright year to 2026 in test files
    This commit updates the copyright year from 2025 to 2026 in multiple test files across the project. The changes ensure that the copyright information is current and reflects the latest year of publication.

--- src/main/java/org/usefultoys/slf4j/NullPrintStream.java
+++ src/main/java/org/usefultoys/slf4j/NullPrintStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Daniel Felix Ferber
+ * Copyright 2026 Daniel Felix Ferber
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
